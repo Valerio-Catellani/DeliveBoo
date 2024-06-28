0d4173fbb43657a805e6234b812b38e70f693b13
Valerio-Catellani <valeriocatellani@gmail.com>
2024-06-28 10:37:35 +0200

    created restaurant seeder

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Restaurant;
 
 class RestaurantSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class RestaurantSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $restaurants = json_decode(file_get_contents(__DIR__ . '\json\Restaurants.json'));
+        foreach ($restaurants as $restaurant) {
+            $new_Restaurant = new Restaurant();
+            $new_Restaurant->name = $restaurant->Name;
+            $new_Restaurant->address = $restaurant->Address;
+            $new_Restaurant->image = $restaurant->image;
+            $new_Restaurant->VAT = $restaurant->VAT;
+            $new_Restaurant->save();
+        }
     }
 }
