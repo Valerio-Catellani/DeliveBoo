6221718e8504132463869db7594538f70133d5ef
Andrea Sgura <andre-56@hotmail.it>
2024-06-28 16:04:27 +0200

    dishseader pt1

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Restaurant;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Dish;
 
 class DishSeeder extends Seeder
 {
@@ -13,7 +14,19 @@ class DishSeeder extends Seeder
      */
     public function run(): void
     {
-        $restaurants = Restaurant::all()->with('categories');
+        // $restaurants = Restaurant::all()->with('categories');
+        $dishes = json_decode(file_get_contents(__DIR__ . '/json/Dishes.json'));
 
+        foreach ($dishes as $dish) {
+            $new_dish = new Dish();
+            $new_dish->name = $dish->name;
+            $new_dish->price = $dish->price;
+            $new_dish->description = $dish->description;
+            $new_dish->image = $dish->image;
+            $new_dish->restaurant_id = $dish->restaurant_id;
+            $new_dish->visible = $dish->visible;
+            $new_dish->ingredients = $dish->ingredients;
+            $new_dish->save();
+        }
     }
 }
