bc97e7242bf7cda730b172cbb0e9d0be1a9a3a87
Valerio-Catellani <valeriocatellani@gmail.com>
2024-07-01 10:54:26 +0200

    added a card

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -2,38 +2,53 @@
     $restaurant = App\Models\Restaurant::where('user_id', Auth::user()->id)->first();
     $user = Auth::user();
     if ($restaurant) {
-    $data = [
-        'restaurant_slug' => $restaurant->slug,
-        'user_slug' => $user->slug,
-    ];
-    };
+        $data = [
+            'restaurant_slug' => $restaurant->slug,
+            'user_slug' => $user->slug,
+        ];
+    }
 @endphp
 
-@section('title', "Dettagli Ristorante: ")
+@section('title', 'Dettagli Ristorante: ')
 @extends('layouts.admin')
 
 
 
 @section('content')
 
-<section class="container py-5">
+    <section class="container py-5">
 
-    <div class="container rounded-2 hype-shadow-white p-0 overflow-hidden example-color">
-    @if($dishes)    
-    <ul>
-            @foreach($dishes as $dish)
-            <li>{{ $dish->name }}</li>
-            @endforeach
-        </ul>
-        
-        @else
-        <p>Non ci sono piatti nel ristorante</p>
-        @endif
-        <a href="{{ route('admin.dishes.create', $data) }}">
-            <div class="btn btn-primary">Crea Nuovo Piatto</div>
-        </a>
-        
-    </div>
+        <div class="container rounded-2 hype-shadow-white p-0 overflow-hidden">
+            @if ($dishes)
+                <div class="row gy-5 p-1">
+                    @foreach ($dishes as $dish)
+                        <div class="col-3">
+                            <div class="card card-custom bg-white border-white border-0">
+                                <div class="card-custom-img" style="background-image: url('{{ $dish->image }}');">
+                                </div>
+                                <div class="card-body" style="overflow-y: auto">
+                                    <h3 class="card-title">{{ $dish->name }}</h3>
+                                    <p class="card-text">Card has minimum height set but will expand if more space is needed
+                                        for card body content. You can use Bootstrap <a
+                                            href="https://getbootstrap.com/docs/4.0/components/card/#card-decks"
+                                            target="_blank">card-decks</a> to align multiple cards nicely in a row.</p>
+                                </div>
+                                <div class="card-footer" style="background: inherit; border-color: inherit;">
+                                    <a href="{{ route('admin.dishes.show', $dish) }}" class="btn btn-primary">Mostra Dettaglio</a>
+                                    <a href="#" class="btn btn-outline-primary">Other option</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            @else
+                <p>Non ci sono piatti nel ristorante</p>
+            @endif
+            <a href="{{ route('admin.dishes.create', $data) }}">
+                <div class="btn btn-primary">Crea Nuovo Piatto</div>
+            </a>
 
-</section>
+        </div>
+
+    </section>
 @endsection
