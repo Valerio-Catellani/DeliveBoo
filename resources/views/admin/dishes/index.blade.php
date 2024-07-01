ea9fbb8f4876cf0d1d2623a67eadad2dd2a98de7
Valerio-Catellani <valeriocatellani@gmail.com>
2024-07-01 16:33:40 +0200

    added controll

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -19,19 +19,24 @@
     <section class="container py-5">
 
         <div class="container rounded-2 hype-shadow-white p-0 overflow-hidden">
+            <h2 class="text-center">Piatti del: {{ $restaurant->name }}</h2>
             @if ($dishes)
                 <div class="row gy-5 p-1">
                     @foreach ($dishes as $dish)
                         <div class="col-3">
                             <div class="card card-custom bg-white border-white border-0">
-                                <div class="card-custom-img" style="background-image: url('{{ $dish->image }}');">
-                                </div>
+                                @if (isset($dish->image) && strpos($dish->image, 'http') === 0)
+                                    <div class="card-custom-img" style="background-image: url('{{ $dish->image }}');">
+                                    </div>
+                                @else
+                                    <div class="card-custom-img"
+                                        style="background-image: url('{{ asset('storage/' . $dish->image) }}');">
+                                    </div>
+                                @endif
                                 <div class="card-body" style="overflow-y: auto">
                                     <h3 class="card-title">{{ $dish->name }}</h3>
-                                    <p class="card-text">Card has minimum height set but will expand if more space is needed
-                                        for card body content. You can use Bootstrap <a
-                                            href="https://getbootstrap.com/docs/4.0/components/card/#card-decks"
-                                            target="_blank">card-decks</a> to align multiple cards nicely in a row.</p>
+                                    <p class="card-text">{{ $dish->description }}</p>
+                                    <p class="card-text">Prezzo: {{ $dish->price }}€</p>
                                 </div>
                                 @php
                                     if ($restaurant) {
@@ -43,15 +48,14 @@
                                     }
 
                                 @endphp
-                                <div class="card-footer" style="background: inherit; border-color: inherit;">
+                                <div class="card-footer d-flex gap-3" style="background: inherit; border-color: inherit;">
                                     <a href="{{ route('admin.dishes.show', $data_dish_slug) }}"
                                         class="btn btn-primary">Mostra Dettaglio</a>
                                     <form id="delete-form" action="{{ route('admin.dishes.destroy', $data_dish_slug) }}"
                                         method="POST">
                                         @csrf
                                         @method('DELETE')
-                                        <button
-                                            class="element-delete default-button text-active-primary hype-text-shadow fs-1 btn btn-danger"
+                                        <button class="element-delete text-active-primary hype-text-shadow btn btn-danger"
                                             type="submit" data-element-id="{{ $dish->id }}"
                                             data-element-title="{{ $dish->name }}">
                                             Elimina Piatto
