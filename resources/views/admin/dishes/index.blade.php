2a4e96f169440f877855385a6e5e33d1ef9d9f61
Andrea Sgura <andre-56@hotmail.it>
2024-07-02 09:46:07 +0200

    crud message

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -14,6 +14,10 @@ if ($restaurant) {
 
 @section('content')
 <section class="container py-5">
+@if(session()->has('message'))
+    <div class="alert alert-success mt-3">{{session()->get('message')}}</div>
+    @endif
+
     <h2 class="text-center">Piatti del: {{ $restaurant->name }}</h2>
     @if ($dishes)
     <div class="row">
@@ -47,7 +51,7 @@ if ($restaurant) {
                     <form id="delete-form" action="{{ route('admin.dishes.destroy', $data_dish_slug) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button class="button-del-main" type="submit" data-element-id="{{ $dish->id }}" data-element-title="{{ $dish->name }}">
+                        <button class="button-del-main element-delete" type="submit" data-element-id="{{ $dish->id }}" data-element-title="{{ $dish->name }}">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 69 14" class="svgIcon bin-top">
                                 <g clip-path="url(#clip0_35_24)">
                                     <path fill="black" d="M20.8232 2.62734L19.9948 4.21304C19.8224 4.54309 19.4808 4.75 19.1085 4.75H4.92857C2.20246 4.75 0 6.87266 0 9.5C0 12.1273 2.20246 14.25 4.92857 14.25H64.0714C66.7975 14.25 69 12.1273 69 9.5C69 6.87266 66.7975 4.75 64.0714 4.75H49.8915C49.5192 4.75 49.1776 4.54309 49.0052 4.21305L48.1768 2.62734C47.3451 1.00938 45.6355 0 43.7719 0H25.2281C23.3645 0 21.6549 1.00938 20.8232 2.62734ZM64.0023 20.0648C64.0397 19.4882 63.5822 19 63.0044 19H5.99556C5.4178 19 4.96025 19.4882 4.99766 20.0648L8.19375 69.3203C8.44018 73.0758 11.6746 76 15.5712 76H53.4288C57.3254 76 60.5598 73.0758 60.8062 69.3203L64.0023 20.0648Z"></path>
