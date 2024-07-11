9360a7f4f6b2b146980ac66f83ee528647d16170
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-07-11 17:03:39 +0200

    restiling resturant details

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -17,42 +17,42 @@
         <div class="row p-3">
             <div class="col-12 col-md-5">
                 <!-- title -->
-                <h1 class="text-left fw-bolder my-3">Scheda di riepilogo ristorante</h1>
+                <h1 class="text-left fw-bolder">Scheda di riepilogo ristorante</h1>
             </div>
             <div class="col-12 col-md-7">
                 <h3>Nome Ristorante</h3>
-                <h5 class="mb-4">{{ $restaurant->name }}</h5>
+                <p class="mb-4 lead">{{ $restaurant->name }}</p>
                 <h3>Indirizzo Ristorante</h3>
-                <h5 class="mb-4">{{ $restaurant->address }}</h5>
+                <p class="mb-4 lead">{{ $restaurant->address }}</p>
                 <h3>Partita IVA</h3>
-                <h5 class="mb-4">{{ $restaurant->VAT }}</h5>
+                <p class="mb-4 lead">{{ $restaurant->VAT }}</p>
                 <h3>Tipologie</h3>
-                @foreach ($typologies as $typology)
-                    <div class="svg-container" style="width: 100px; aspect-ratio: 1/1">
-                        {!! $typology->icon !!}
-                    </div>
-                    <h2><span class="badge text-bg-secondary"
-                            style="background-color:{{ $typology->color }} !important">{{ $typology->name }}</span>
-                    </h2>
-                @endforeach
-            </div>
-        </div>
-    </div>
+                <div class="d-flex">
+                    @foreach ($typologies as $typology)
+                        <div class="row gap-2">
+                            <div class="svg-container debug" style="width: 100px; aspect-ratio: 1/1">{!! $typology->icon !!}
+                            </div>
+                            <div class="badge text-bg-secondary" style="background-color:{{ $typology->color }} !important">
+                                {{ $typology->name }}
+                            </div>
+                        </div>
 
-    <div class="container">
-        <div class="row gap-3 p-3">
-
-            <div class="col-6 d-flex flex-column text-white">
-
-                <div class="d-flex justify-content-center align-items-center gap-5 mt-auto">
-                    <a class="btn btn-light" href="{{ route('admin.dashboard', Auth::user()->slug) }}">
-                        Torna alla HomePage
-                    </a>
-
+                    @endforeach
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="container mt-3">
+        <div class="row gap-3 p-3">
+            <div class="d-flex justify-content-center align-items-center mt-auto">
+                <a class="btn btn-light" href="{{ route('admin.dashboard', Auth::user()->slug) }}">
+                    Torna alla HomePage
+                </a>
+            </div>
+        </div>
     </div>
+
+
 </section>
 @endsection
