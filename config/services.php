9ee542c6e01e70d41b1d000ba3c422cf4cd424e3
Valerio-Catellani <valeriocatellani@gmail.com>
2024-07-14 19:29:18 +0200

    minor fix

--- config/services.php
+++ config/services.php
@@ -26,11 +26,11 @@ return [
     ],
 
     'braintree' => [
-    'environment' => env('BRAINTREE_ENVIRONMENT'),
-    'merchantId' => env('BRAINTREE_MERCHANT_ID'),
-    'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
-    'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
-],
+        'environment' => env('BRAINTREE_ENVIRONMENT', 'sandbox'),
+        'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+        'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+        'privateKey' => env('BRAINTREE_PRIVATE_KEY'),
+    ],
 
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
